a36f16bc80ace113ad6d2ac7983281ef65adcafb
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-04-08 11:18:29 +0300

    lab6: Add solution for exercise 0 - Recapitulare - Descompunerea unui numar in puteri ale lui 2
    Signed-off-by: Diana Cismaru <cismaru_diana02@yahoo.com>

--- laborator/content/rolul-registrelor-adresare/0-recap/power-2.asm
+++ laborator/content/rolul-registrelor-adresare/0-recap/power-2.asm
@@ -9,8 +9,18 @@ main:
 
     mov eax, 211    ; to be broken down into powers of 2
     mov ebx, 1      ; stores the current power
-
-    ; TODO - print the powers of 2 that generate number stored in EAX
+    mov esi, 0  ; contor
+    mov ecx, 1  ; aici pun masca
+start:
+    test eax, ecx ; fac SI logic
+    jz next
+    PRINTF32 `%d \x0`, ebx
+next:
+    shl ecx, 1  ; shiftez masca la stanga cu 1 ca sa compar bitii
+    shl ebx, 1  ; inmultesc cu 2 de fiecare data
+    inc esi     ; cresc contorul
+    cmp esi, 32 ; aici trebuie sa opresc
+    jne start   ; daca inca nu a ajuns la 32, continui programul
 
     leave
     ret
