d31868b021d24d371d6520a9affb8e0d84818e36
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-04-08 11:35:26 +0300

    lab6: Optimize solution for exercise 0
    Remove register ebx
    
    Signed-off-by: Diana Cismaru <cismaru_diana02@yahoo.com>

--- laborator/content/rolul-registrelor-adresare/0-recap/power-2.asm
+++ laborator/content/rolul-registrelor-adresare/0-recap/power-2.asm
@@ -8,7 +8,6 @@ main:
     mov ebp, esp
 
     mov eax, 211    ; to be broken down into powers of 2
-    mov ebx, 1      ; stores the current power
     mov esi, 0  ; contor
     mov ecx, 1  ; aici pun masca
 start:
@@ -17,7 +16,6 @@ start:
     PRINTF32 `%d \x0`, ebx
 next:
     shl ecx, 1  ; shiftez masca la stanga cu 1 ca sa compar bitii
-    shl ebx, 1  ; inmultesc cu 2 de fiecare data
     inc esi     ; cresc contorul
     cmp esi, 32 ; aici trebuie sa opresc
     jne start   ; daca inca nu a ajuns la 32, continui programul
