91c8734f95c13fbf1a206044a4464aafaaf00956
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-05-13 13:23:39 +0300

    lab9: Add solution for exercise 4-5-print-string-len
    Signed-off-by: Diana Cismaru <cismaru_diana02@yahoo.com>

--- laborator/content/apel-functii/4-5-print-string-len/print-string-len.asm
+++ laborator/content/apel-functii/4-5-print-string-len/print-string-len.asm
@@ -2,6 +2,7 @@
 
 section .data
     mystring db "This is my string", 0
+    print_format db "String length is %d", 10, 0
 
 section .text
 
@@ -18,13 +19,19 @@ main:
 test_one_byte:
     mov bl, byte [eax]
     test bl, bl
-    je out
+    jz out
     inc eax
     inc ecx
     jmp test_one_byte
 
 out:
     PRINTF32 `[PRINTF32]: %d\n[printf]: \x0`, ecx
+
+print:
+    push ecx
+    push print_format
+    call printf
+    add esp, 8
 
     leave
     ret
