bc311e70e9026294ddfd5945b5d3ff53044e6b24
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-05-14 02:01:09 +0300

    lab9: Add solution for exercise 6-toupper       Add solution for BONUS toupper       Add solution for BONUS rot13
    Signed-off-by: Diana Cismaru <cismaru_diana02@yahoo.com>

--- laborator/content/apel-functii/6-toupper/toupper.asm
+++ laborator/content/apel-functii/6-toupper/toupper.asm
@@ -1,7 +1,8 @@
 section .data
     before_format db "before %s", 13, 10, 0
     after_format db "after %s", 13, 10, 0
-    mystring db "abcdefghij", 0
+    rot13_format db "rot13 %s", 13, 10, 0
+    mystring db "abcdeFGhij", 0
 
 section .text
 
@@ -12,10 +13,61 @@ toupper:
     push ebp
     mov ebp, esp
 
-    ; TODO
+    mov eax, dword [ebp + 8]
+    check_letter:
+        mov bl, byte [eax]
+        cmp bl, 0
+        je finish
 
-    leave
-    ret
+        ;; START BONUS 1 ;;
+        cmp bl, 'a'
+        jl skip_letter
+        cmp bl, 'z'
+        jg skip_letter
+        ;; END BONUS 1 ;;
+
+        sub bl, 32
+        mov byte [eax], bl
+        skip_letter:
+            inc eax
+            jmp check_letter
+    finish:
+        leave
+        ret
+
+;; START BONUS 2 ;;
+rot13:
+    push ebp
+    mov ebp, esp
+    mov eax, dword [ebp + 8]
+    iterate:
+        mov bl, byte [eax]
+        cmp bl, 0
+        je end
+
+        cmp bl, 'n'
+        jge substitution
+        cmp bl, 'a'
+        jge addition
+        cmp bl, 'N'
+        jge substitution
+        
+        addition:
+            add bl, 13
+            jmp continue
+        substitution:
+            sub bl, 13
+
+        continue:
+            mov byte [eax], bl
+            inc eax
+            jmp iterate
+    
+    end:
+        leave
+        ret
+
+;; END BONUS 2 ;;
 
 main:
     push ebp
@@ -32,6 +84,16 @@ main:
 
     push mystring
     push after_format
+    call printf
+    add esp, 8
+
+    ;; call bonus 2
+    push mystring
+    call rot13
+    add esp, 4
+
+    push mystring
+    push rot13_format
     call printf
     add esp, 8
 
